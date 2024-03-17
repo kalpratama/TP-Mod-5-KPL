1e00f9eaff693f9f2ca48da36a618dcc0ef8e4ac
Kal EL Pratama <152152274+kalpratama@users.noreply.github.com>
2024-03-17 22:12:29 +0700

    Add files via upload

---
+++ TPMod5_1302220096/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public T Data
+    {
+        get {return data;}
+        set {data = value;}
+    }
+
+    public void PrintData()
+    {
+        System.Console.WriteLine($"Data yang tersimpan adalah: {data}");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Kal");
+
+        string NIM = "1302220096";
+        DataGeneric<string> dataString = new DataGeneric<string>(NIM);
+        dataString.PrintData();
+    }
+}
